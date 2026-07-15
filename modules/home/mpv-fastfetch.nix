1a92de6f31ba696c93e30a0572afdab1ecf7768d
inecekk <164870142@qq.com>
2026-07-15 14:59:36 +0800

    gen-571: 07-15_14:59 (29s)

--- modules/home/mpv-fastfetch.nix
+++ modules/home/mpv-fastfetch.nix
@@ -1,8 +1,7 @@
 # modules/home/mpv-fastfetch.nix
-{ pkgs, ... }: 
+{ pkgs, ... }:
 
 {
-
   programs.mpv = {
     enable = true;
     config = {
@@ -16,45 +15,39 @@
       "WHEEL_UP" = "add volume 5";
       "WHEEL_DOWN" = "add volume -5";
     };
-    
-    scripts = [
-      pkgs.mpvScripts.mpris
-    ];
+    scripts = [ pkgs.mpvScripts.mpris ];
   };
 
   programs.fastfetch = {
     enable = true;
     settings = {
-      logo = { type = "auto"; padding = { top = 1; right = 4; }; };
-      display = { separator = "  "; };
-	modules = [
+      logo = {
+        type = "auto";
+        padding = { top = 1; right = 4; };
+      };
+      display.separator = " 󰁔 ";
+      modules = [
         "title"
         "separator"
-        { type = "os"; key = "󱄅 系统"; }
-        { type = "kernel"; key = "󰒋 内核"; }
-        { type = "uptime"; key = "󰅐 开机时长"; }
-        { type = "packages"; key = "󰏖 软件包"; }
-        
-        # 新增：中文环境/语言设置
-        { type = "locale"; key = "󰇄 语言"; }
-        # 新增：虚拟化检测
-        { type = "initsystem"; key = "󰣖 systemd"; }
-        { type = "terminal"; key = " 终端"; }
-        { type = "terminalfont"; key = " 字体"; }
-        
+        { type = "os"; key = "󱄅 OS"; }
+        { type = "kernel"; key = "󰒋 Kernel"; }
+        { type = "host"; key = "󰇅 Host"; }
+        { type = "uptime"; key = "󰅐 Uptime"; }
+        { type = "packages"; key = "󰏖 Packages"; }
+        { type = "wm"; key = "󰖯 WM"; }
+        { type = "de"; key = "󰧨 DE"; }
+        { type = "terminal"; key = " Terminal"; }
         "break"
         { type = "cpu"; key = " CPU"; }
-        { type = "memory"; key = " 内存"; format = "{percentage} ({used} / {total})"; }
+        { type = "gpu"; key = "󰢮 GPU"; }
+        { type = "memory"; key = " Memory"; format = "{percentage} ({used}/{total})"; }
         "break"
-        "disk"
-        "break"
-        { type = "display"; key = "󰍹 分辨率"; }
-        { type = "localip"; key = "󰩟 内网IP"; showIpv4 = true; }
-        { type = "publicip"; key = "󰖟 公网IP"; }
+        { type = "display"; key = "󰍹 Display"; }
+        { type = "disk"; key = "󰋊 Disk"; }
+        { type = "localip"; key = "󰩟 IP"; showIpv4 = true; }
         "break"
         { type = "colors"; symbol = "block"; }
       ];
-
     };
   };
 }
