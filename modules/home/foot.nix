2670595254f4eefccacd5b02d3d75dcefbc75f52
inecekk <164870142@qq.com>
2026-07-15 12:48:01 +0800

    gen-566: 07-15_12:48 (耗时 26s)

--- modules/home/foot.nix
+++ modules/home/foot.nix
@@ -7,7 +7,7 @@
   xdg.configFile."foot/foot.ini".text = ''
     [main]
     # 字体与大小
-    font=JetBrains Mono Nerd Font:size=12
+    font=JetBrains Mono,Symbols Nerd Font Mono:size=12
     # 内边距，4x4 像素，居中
     pad=4x4 center
     # 选中文本后自动复制到系统剪贴板
