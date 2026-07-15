3c2a52c703b0bdebc6b5ab2f60067c3683a8f481
inecekk <164870142@qq.com>
2026-07-15 15:24:10 +0800

    gen-576: 07-15_15:24 (31s)

--- modules/home/mpv-fastfetch.nix
+++ modules/home/mpv-fastfetch.nix
@@ -1,6 +1,5 @@
 # modules/home/mpv-fastfetch.nix
 { pkgs, ... }:
-
 {
   programs.mpv = {
     enable = true;
@@ -22,41 +21,37 @@
     enable = true;
     settings = {
       logo = {
-        type = "auto";
+        type = "raw";
+        source = ''
+${"\u001b[38;5;99m"} _   _ _      ___  ____
+${"\u001b[38;5;98m"}| \ | (_)_  _/ _ \/ ___|
+${"\u001b[38;5;68m"}|  \| | \ \/ / | | \___ \
+${"\u001b[38;5;44m"}| |\  | |>  <| |_| |___) |
+${"\u001b[38;5;43m"}|_| \_|_/_/\_\\___/|____/
+${"\u001b[0m"}
+'';
         padding = { top = 1; right = 4; };
       };
-
       display.separator = " ";
       modules = [
-  "title"
-  "separator"
-
-  { type = "os"; key = "󱄅"; keyColor = "magenta"; }
-  { type = "kernel"; key = "󰒋"; keyColor = "blue"; }
-  { type = "uptime"; key = "󰅐"; keyColor = "green"; }
-  { type = "packages"; key = "󰏖"; keyColor = "yellow"; }
-
-  { type = "wm"; key = "󰖯"; keyColor = "magenta"; }
-  { type = "shell"; key = ""; keyColor = "cyan"; }
-  { type = "terminal"; key = ""; keyColor = "blue"; }
-
-  "break"
-
-  { type = "cpu"; key = ""; keyColor = "green"; }
-  { type = "gpu"; key = "󰢮"; keyColor = "cyan"; }
-  { type = "memory"; key = ""; keyColor = "yellow"; }
-
-  "break"
-
-  { type = "disk"; key = "󰋊"; folders = [ "/" ]; keyColor = "blue"; }
-  { type = "display"; key = "󰍹"; keyColor = "magenta"; }
-  { type = "localip"; key = "󰩟"; keyColor = "cyan"; showIpv4 = true; }
-
-  "break"
-
-  { type = "colors"; symbol = "block"; }
-]; 
-
+        { type = "os"; key = "󱄅"; keyColor = "magenta"; }
+        { type = "kernel"; key = "󰒋"; keyColor = "blue"; }
+        { type = "uptime"; key = "󰅐"; keyColor = "green"; }
+        { type = "packages"; key = "󰏖"; keyColor = "yellow"; }
+        { type = "wm"; key = "󰖯"; keyColor = "magenta"; }
+        { type = "shell"; key = ""; keyColor = "cyan"; }
+        { type = "terminal"; key = ""; keyColor = "blue"; }
+        "break"
+        { type = "cpu"; key = ""; keyColor = "green"; }
+        { type = "gpu"; key = "󰢮"; keyColor = "cyan"; }
+        { type = "memory"; key = ""; keyColor = "yellow"; }
+        "break"
+        { type = "disk"; key = "󰋊"; folders = [ "/" ]; keyColor = "blue"; }
+        { type = "display"; key = "󰍹"; keyColor = "magenta"; }
+        { type = "localip"; key = "󰩟"; keyColor = "cyan"; showIpv4 = true; }
+        "break"
+        { type = "colors"; symbol = "block"; }
+      ];
     };
   };
 }
