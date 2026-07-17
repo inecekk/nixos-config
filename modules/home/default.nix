718a96e448fb6d4ed5c501b1fd6fc09c73f57231
inecekk <164870142@qq.com>
2026-07-17 17:36:04 +0800

    gen-607: 07-17_17:36 (15s)

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,14 +1,26 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
+let
+        homeDir = ./.;
+
+        # 判断一个目录条目是否应该被自动 import：
+        # 1. 必须是普通文件（排除目录、软链接等）
+        # 2. 文件名以 .nix 结尾（.bak 后缀的备份文件会被天然排除，因为 "xxx.nix.bak" 不以 ".nix" 结尾）
+        # 3. 排除 default.nix 自己，避免自我引用
+        isAutoImportable = name: type:
+        type == "regular"
+        && lib.hasSuffix ".nix" name
+        && name != "default.nix";
+
+        # 扫描当前目录，生成所有符合条件的文件路径列表
+        autoImports =
+        builtins.map
+        (name: homeDir + "/${name}")
+        (builtins.attrNames (lib.filterAttrs isAutoImportable (builtins.readDir homeDir)));
+in
 {
         home-manager.users.lk = { pkgs, ... }: {
-        imports = [
-        ./niri.nix ./foot.nix ./fcitx5-rime.nix ./mpv-fastfetch.nix
-        ./mako-mpd.nix ./rnote.nix ./noctalia.nix
-        #./yambar.nix	
-        ];
-
+        imports = autoImports;
         home.stateVersion = "26.11";
-
         # --- 用户应用软件包 ---
         home.packages = with pkgs; [
         git gnused tree wget foot bluetui
@@ -23,7 +35,6 @@
         # 开发生产力：
         vscode rnote opentabletdriver
         ];
-
         # --- 用户级环境变量 ---
         home.sessionVariables = {
         XMODIFIERS = "@im=fcitx";
@@ -35,7 +46,6 @@
         GDK_BACKEND = "wayland";
         SDL_VIDEODRIVER = "wayland";
         };
-
         # --- 用户级服务 ---
         systemd.user.services.blueman-applet = {
         Unit = { Description = "Disabled Blueman Applet"; };
