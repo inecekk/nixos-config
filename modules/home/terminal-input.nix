1baccaf57da91c25cde8a144078b6f939b33f522
inecekk <164870142@qq.com>
2026-07-24 17:46:54 +0800

    gen-724 07-24_17:46 33s

--- modules/home/terminal-input.nix
+++ modules/home/terminal-input.nix
@@ -1,33 +1,66 @@
 # modules/home/terminal-input.nix
+# ==========================================
+# 终端与输入法:foot + fcitx5
+# ==========================================
 { pkgs, ... }:
 {
+  # ---------- foot 终端 ----------
   xdg.configFile."foot/foot.ini".text = ''
     [main]
     font=JetBrains Mono:size=10, WenQuanYi Micro Hei:size=10
     dpi-aware=yes
     pad=3x1 center
     selection-target=clipboard
+    horizontal-letter-offset=0
+    vertical-letter-offset=0
+    resize-delay-ms=10
+    word-delimiters= ,│`|:"'()[]{}<>@%
+    bold-text-in-bright=yes
+    [scrollback]
+    lines=10000
     [csd]
     preferred=none
     [colors-dark]
     alpha=0.65
     foreground=e0e0e0
     background=000000
+    cursor=000000 e0e0e0
+    selection-background=000000
+    selection-foreground=ffffff
+    regular0=2e3436
+    regular1=cc0000
+    regular2=4e9a06
+    regular3=c4a000
+    regular4=3465a4
+    regular5=75507b
+    regular6=06989a
+    regular7=d3d7cf
+    bright0=555753
+    bright1=ef2929
+    bright2=8ae234
+    bright3=fce94f
+    bright4=729fcf
+    bright5=ad7fa8
+    bright6=34e2e2
+    bright7=eeeeec
     [bell]
     notify=no
     visual=no
     [cursor]
     style=block
     blink=no
+    [mouse]
+    hide-when-typing=yes
   '';
 
+  # ---------- fcitx5 输入法(rime-ice + 小鹤双拼)----------
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5 = {
       waylandFrontend = true;
       addons = with pkgs; [
-        fcitx5-rime
         qt6Packages.fcitx5-chinese-addons
+        (fcitx5-rime.override { rimeDataPkgs = [ rime-ice ]; })
       ];
     };
   };
@@ -36,78 +69,16 @@
     force = true;
     text = ''
       patch:
+        __include: rime_ice_suggestion:/
         schema_list:
           - schema: double_pinyin_flypy
-        switcher/hotkeys: [F4]
+        switcher/hotkeys:
+          - F4
         menu/page_size: 5
     '';
   };
 
-  xdg.configFile."fcitx5/rime/double_pinyin_flypy.schema.yaml" = {
-    force = true;
-    text = ''
-      schema:
-        schema_id: double_pinyin_flypy
-        name: 小鹤双拼
-      switches:
-        - name: ascii_mode
-          states: [中文, 西文]
-          reset: 0
-      engine:
-        processors: [ascii_composer, recognizer, key_binder, speller, punctuator, selector, navigator, express_editor]
-        segmentors: [ascii_segmentor, matcher, abc_segmentor, punct_segmentor, fallback_segmentor]
-        translators: [punct_translator, table_translator]
-        filters: [simplifier, uniquifier]
-      speller:
-        alphabet: "zyxwvutsrqponmlkjihgfedcba;"
-        delimiter: " '"
-        algebra:
-          - erase/^xx$/
-          - derive/^([jqxy])u$/$1v/
-          - xlit/ch/i/
-          - xlit/sh/u/
-          - xlit/zh/v/
-          - xform/^([a-z])ai$/$1l/
-          - xform/^([a-z])an$/$1j/
-          - xform/^([a-z])ang$/$1h/
-          - xform/^([a-z])ao$/$1k/
-          - xform/^([a-z])ei$/$1z/
-          - xform/^([a-z])en$/$1f/
-          - xform/^([a-z])eng$/$1g/
-          - xform/^([a-z])ia$/$1x/
-          - xform/^([a-z])ian$/$1m/
-          - xform/^([a-z])iang$/$1d/
-          - xform/^([a-z])iao$/$1c/
-          - xform/^([a-z])ie$/$1p/
-          - xform/^([a-z])in$/$1n/
-          - xform/^([a-z])ing$/$1y/
-          - xform/^([a-z])iong$/$1s/
-          - xform/^([a-z])iu$/$1q/
-          - xform/^([a-z])ong$/$1s/
-          - xform/^([a-z])ou$/$1b/
-          - xform/^([a-z])ua$/$1x/
-          - xform/^([a-z])uai$/$1k/
-          - xform/^([a-z])uan$/$1r/
-          - xform/^([a-z])uang$/$1d/
-          - xform/^([a-z])ue$/$1t/
-          - xform/^([a-z])ui$/$1v/
-          - xform/^([a-z])un$/$1p/
-          - xform/^([a-z])uo$/$1o/
-          - xform/^([a-z])ve$/$1t/
-          - abbrev/^([a-z]).+$/$1/
-      translator:
-        dictionary: luna_pinyin
-        enable_sentence: true
-        enable_user_dict: true
-      punctuator:
-        import_preset: default
-      key_binder:
-        import_preset: default
-      recognizer:
-        import_preset: default
-    '';
-  };
-
+  # ---------- 关闭日志,减少 I/O 开销 ----------
   home.sessionVariables = {
     QT_IM_MODULE = "fcitx";
     GLOG_minloglevel = "3";
