95fe1624f532cf5925a238a2dacb37c664a47fcb
inecekk <164870142@qq.com>
2026-07-24 11:14:19 +0800

    gen-705: 07-24_11:14 (33s)

--- modules/home/ytmusic.nix
+++ modules/home/ytmusic.nix
@@ -4,6 +4,9 @@ let
   ytmusic = pkgs.writeShellScriptBin "ytmusic" ''
     query="$*"
 
+	if [ -z "$query" ]; then
+  	query="YouTube Music Top Songs"
+	fi
     ${pkgs.yt-dlp}/bin/yt-dlp \
       --cookies-from-browser "firefox:/home/lk/.config/zen/tso70vj4.Default Profile" \
       --print "%(title)s|%(webpage_url)s" \
