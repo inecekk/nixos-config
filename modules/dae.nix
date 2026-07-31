a7b59a0a59d96a71ca9d7eb6eae6f08840ce7c49
inecekk <164870142@qq.com>
2026-07-31 12:49:35 +0800

    upgrade gen-759 07-31_12:49

--- modules/dae.nix
+++ modules/dae.nix
@@ -5,12 +5,16 @@
   ...
 }: {
   # ----------------------------------------------------
+  # dae 服务主体配置
+  # ----------------------------------------------------
   services.dae = {
     enable = true;
-
-    # 原生使用 pkgs.dae
     package = pkgs.dae;
-    # package = pkgs.callPackage ../pkgs/dae-v2.nix {}; # 注释掉自定义 dae-v2
+
+    assets = with pkgs; [
+      v2ray-geoip
+      v2ray-domain-list-community
+    ];
 
     openFirewall = {
       enable = true;
@@ -18,12 +22,8 @@
     };
 
     configFile = "/etc/dae/config.dae";
-
-    assetsPath = "${pkgs.v2ray-geoip}/share/v2ray:${pkgs.v2ray-domain-list-community}/share/v2ray";
   };
 
-  # ----------------------------------------------------
-  # Systemd 服务重写（延迟启动与 cgroup 内存限制）
   # ----------------------------------------------------
   systemd.services.dae = {
     wantedBy = lib.mkForce [];
@@ -37,15 +37,20 @@
       "network-online.target"
     ];
 
-    # 内存上限控制
+    preStart = ''
+      mkdir -p /run/credentials/dae.service
+      ln -sf ${pkgs.v2ray-geoip}/share/v2ray/geoip.dat /run/credentials/dae.service/geoip.dat
+      ln -sf ${pkgs.v2ray-domain-list-community}/share/v2ray/geosite.dat /run/credentials/dae.service/geosite.dat
+    '';
+
     serviceConfig = {
-      MemoryMax = "70M";       # 内存上限,超出直接被 cgroup 干掉
-      MemoryHigh = "90M";      # 软上限,超过后内核会尝试回收
+      MemoryHigh = "70M";
+      MemoryMax = "90M";
     };
   };
 
   # ----------------------------------------------------
-  # 4. 开机 9 秒延迟启动 Timer
+  # 开机 9 秒延迟启动 Timer
   # ----------------------------------------------------
   systemd.timers.dae-delayed = {
     wantedBy = [
@@ -53,7 +58,7 @@
     ];
 
     timerConfig = {
-      OnBootSec = "9s";        # 开机 9 秒后启动
+      OnBootSec = "9s";
       Unit = "dae.service";
     };
   };
